0b1ed01e0ecbff068fc593ef903be32c01586e2f
DHIA-HANNAH-20031826 <20031826@rp.edu.sg>
2021-07-29 13:00:49 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/p05_ndpsongs/CustomAdapter.java
@@ -0,0 +1,68 @@
+package com.myapplicationdev.android.p05_ndpsongs;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter {
+
+    Context parent_context;
+    int layout_id;
+    ArrayList<Song> List;
+
+    public CustomAdapter(Context context, int resource, ArrayList<Song> objects) {
+
+        super(context, resource, objects);
+
+        parent_context = context;
+        layout_id = resource;
+        List = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Obtain the LayoutInflater object
+        LayoutInflater inflater = (LayoutInflater)
+                parent_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // "Inflate" the View for each row
+        View rowView = inflater.inflate(layout_id, parent, false);
+
+        // Obtain the UI components and do the necessary binding
+        TextView tvTitle = rowView.findViewById(R.id.textViewTitle);
+        TextView tvDate = rowView.findViewById(R.id.textViewDate);
+        TextView tvSinger = rowView.findViewById(R.id.textViewSinger);
+//        TextView tvStar = rowView.findViewById(R.id.textViewStars);
+        ImageView ivNew = rowView.findViewById(R.id.imageViewNew);
+        RatingBar rb = rowView.findViewById(R.id.ratingBar2);
+        RatingBar rb2 = rowView.findViewById(R.id.ratingBar3);
+
+        // Obtain the Android Version information based on the position
+        Song currentVersion = List.get(position);
+
+        // Set values to the TextView to display the corresponding information
+        tvTitle.setText(currentVersion.getTitle());
+        tvDate.setText(currentVersion.getYearReleased());
+        tvSinger.setText(currentVersion.getSingers());
+//        tvStar.setText(currentVersion.getStars());
+        rb.setNumStars(currentVersion.getStars());
+        rb2.setNumStars(currentVersion.getStars());
+
+        if(currentVersion.getYearReleased() <= 2019) {
+            ivNew.setImageResource(R.drawable.newsong);
+        } else {
+            ivNew.setVisibility(View.GONE);
+        }
+
+        return rowView;
+    }
+
+}
+
